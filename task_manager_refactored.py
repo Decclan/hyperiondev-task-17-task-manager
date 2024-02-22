298df3de90eb9e210c1548e02db62a9f4d156d9f
Decclan <dec.evan11@gmail.com>
2024-02-22 19:49:51 +0000

    edit task function complete

--- task_manager_refactored.py
+++ task_manager_refactored.py
@@ -20,10 +20,14 @@ def word_input(prompt):
     while True:
         word = input(prompt)
         # Regex check for basic sentence characters.
-        invalid_word = re.search("[^A-Za-z0-9 ,.$£:;*#-+%=!]+",word)
+        invalid_word = re.search("[^A-Za-z0-9 ,.$£:;*#+%=!\-]+",word)
+        # You have to escape the minus sign using backslash.
+        # var alphaExp = /^[a-zA-ZåäöÅÄÖ\s\-]+$/;
+        # To stop them from using it in the beginning or the end, try something like this:
+        # var alphaExp = /^[a-zA-ZåäöÅÄÖ\s]+[a-zA-ZåäöÅÄÖ\s\-]*[a-zA-ZåäöÅÄÖ\s]+$/;
 
         if invalid_word:
-            print("Only special characters , . $ £ : ; * # - + % = ! will be accepted.")
+            print("Only special characters , . $ £ : ; * # + % = ! - will be accepted.")
         # If empty
         elif word == "":
             print("Please enter a value.")
@@ -49,15 +53,13 @@ def password_validation(prompt):
             special = re.search("[^@#$£%^&+=!]+",try_password)
             if upper and lower and number and special:
                 return try_password
-            else:
-                print("""\nInvalid input.
+        else:
+            print("""\nInvalid input.
 Please enter at least:
 one capital letter
 one lowercase letter
 one number
 one special character: @ # $ £ % ^ & + = !\n""")
-        else:
-            print("Invalid password.")
 #===================================================================================================
 
 def task_file():
@@ -65,7 +67,9 @@ def task_file():
     if not os.path.exists("tasks.txt"):
         with open("tasks.txt", "w", encoding="utf-8") as default_task_file:
             #default_task_file.write("Task List")
-            default_task_file.write("admin;Add functionality to task manager;Add additional options and refactor the code.;2022-12-01;2022-11-22;No")
+            default_task_file.write("admin;first task;fix deletion of file if task not added;2024-03-14;2024-02-21;No")
+            #User;Task;Task Description;Due Date;Assigned Date;Completed:
+            #decclan;first task;fix deletion of file if task not added;2024-03-14;2024-02-21;No
 
     with open("tasks.txt", 'r', encoding="utf-8") as task_file:
         task_data = task_file.read().split("\n")
@@ -104,10 +108,10 @@ def user_log_in():
         user_data = user_file.read().split("\n")
 
     # Convert to a dictionary
-    username_password = {}
+    all_users_dict = {}
     for user in user_data:
         username, password = user.split(';')
-        username_password[username] = password
+        all_users_dict[username] = password
 
     logged_in = False
     while not logged_in:
@@ -119,67 +123,62 @@ def user_log_in():
             curr_pass = input("Admin Password: ")
         else:
             curr_pass = password_validation("Password: ")
-        if curr_user not in username_password: ## [consider-iterating-dictionary]
+        if curr_user not in all_users_dict:
             print("User does not exist")
             continue
-        elif username_password[curr_user] != curr_pass:
+        elif all_users_dict[curr_user] != curr_pass:
             print("Wrong password")
             continue
         else:
             print("Login Successful!")
             logged_in = True
-            return username_password, curr_user
+            return curr_user, all_users_dict
 #===================================================================================================
 
-def reg_user(username_password):
+def reg_user(original_user_dict):
     '''Add a new user to the user.txt file'''
-    # - Request input of a new username
-    new_username = word_input("New Username: ")
+    while True:
+        try:
+            # - Request input of a new username
+            new_username = word_input("New Username: ")
 
-    # - Request input of a new password
-    new_password = password_validation("New Password: ")
+            # - Request input of a new password
+            new_password = password_validation("New Password: ")
 
-    # - Request input of password confirmation.
-    confirm_password = password_validation("Confirm Password: ")
+            # - Request input of password confirmation.
+            confirm_password = password_validation("Confirm Password: ")
 
-    # - Check if user already exists in dictionary
-    if new_username not in username_password:
-        # - Check if the new password and confirmed password are the same.
-        if new_password == confirm_password:
-            # - If they are the same, add them to the user.txt file,
-            print(f"New user: {new_username} successfully added.")
-            username_password[new_username] = new_password
+            # - Check if user already exists in dictionary
+            if new_username not in original_user_dict:
+                # - Check if the new password and confirmed password are the same.
+                if new_password == confirm_password:
+                    # - If they are the same, add them to the user.txt file,
+                    print(f"New user: {new_username} successfully added.")
+                    original_user_dict[new_username] = new_password
 
-            with open("user.txt", "w", encoding="utf-8") as out_file:
-                new_user_data = []
-                for k in username_password:
-                    new_user_data.append(f"{k};{username_password[k]}")
-                out_file.write("\n".join(new_user_data))
+                    with open("user.txt", "w", encoding="utf-8") as out_file:
+                        new_user_data = []
+                        for k in original_user_dict:
+                            new_user_data.append(f"{k};{original_user_dict[k]}")
+                        out_file.write("\n".join(new_user_data))
+                    break
 
-        # - Otherwise you present a relevant message and rerun if attempt fails.
-        else:
-            print("Passwords do no match.")
-            reg_user(username_password)
-    else:
-        print("This user already exists.")
-        reg_user(username_password)
+                # - Otherwise you present a relevant message and rerun if attempt fails.
+                else:
+                    print("Passwords do no match.")
+            else:
+                print("This user already exists.")
+        except ValueError:
+            print("Invalid input.")
 #===================================================================================================
 
-def add_task(username_password, task_list):
+def add_task(username_password, main_task_list):
     '''Allow a user to add a new task to task.txt file
         Prompt a user for the following: 
         - A username of the person whom the task is assigned to,
         - A title of a task,
         - A description of the task and 
         - the due date of the task.'''
-
-    task_username = word_input("Name of person assigned to task: ")
-
-    if task_username not in username_password.keys():
-        print("User does not exist. Please enter a valid username")
-        #continue
-    task_title = word_input("Title of Task: ")
-    task_description = word_input("Description of Task: ")
 
     # Date object from todays date
     curr_date = date.today()
@@ -190,15 +189,24 @@ def add_task(username_password, task_list):
 
     while True:
         try:
-            task_due_date = input("Due date of task (YYYY-MM-DD): ")
-            print(curr_date)
+            task_username = word_input("Name of person assigned to task: ")
 
-            due_date_time = datetime.strptime(task_due_date, DATETIME_STRING_FORMAT)
-            # Due date validation
-            if due_date_time < curr_date:
-                print("Must be due today or after todays date")
+            if task_username not in username_password.keys():
+                print("User does not exist. Please enter a valid username")
+
             else:
-                break
+                task_title = word_input("Title of Task: ")
+                task_description = word_input("Description of Task: ")
+
+                task_due_date = input("Due date of task (YYYY-MM-DD): ")
+                print(curr_date)
+
+                due_date_time = datetime.strptime(task_due_date, DATETIME_STRING_FORMAT)
+                # Due date validation
+                if due_date_time < curr_date:
+                    print("Must be due today or after todays date")
+                else:
+                    break
 
         except ValueError:
             print("Invalid datetime format. Please use the format specified")
@@ -209,73 +217,120 @@ def add_task(username_password, task_list):
         "username": task_username,
         "title": task_title,
         "description": task_description,
-        "due_date": due_date_time,
+        "due_date": due_date_time, #due_date_time, adds 00:00:00 time stamp try string_date,
         "assigned_date": curr_date,
         "completed": False
     }
+    add_task_to_file(main_task_list, new_task)
+#===================================================================================================
 
-    task_list.append(new_task)
-    with open("tasks.txt", "w", encoding="utf-8") as add_task_file:
-        task_list_to_write = []
-        for t in task_list:
-            str_attrs = [
-                t['username'],
-                t['title'],
-                t['description'],
-                t['due_date'].strftime(DATETIME_STRING_FORMAT),
-                t['assigned_date'].strftime(DATETIME_STRING_FORMAT),
-                "Yes" if t['completed'] else "No"
-            ]
-            task_list_to_write.append(";".join(str_attrs))
-        add_task_file.write("\n".join(task_list_to_write))
-    print("Task successfully added.")
+def add_task_to_file(main_task_list, task):
+    """Add task to main list of tasks"""
+    try:
+        if task not in main_task_list:
+            main_task_list.append(task)
+        with open("tasks.txt", "w", encoding="utf-8") as add_task_file:
+            task_list_to_write = []
+            for t in main_task_list:
+                str_attrs = [
+                    t['username'],
+                    t['title'],
+                    t['description'],
+                    t['due_date'].strftime(DATETIME_STRING_FORMAT),
+                    t['assigned_date'].strftime(DATETIME_STRING_FORMAT),
+                    "Yes" if t['completed'] else "No"
+                ]
+                task_list_to_write.append(";".join(str_attrs))
+            add_task_file.write("\n".join(task_list_to_write))
+        print("Task successfully added.")
+    except ValueError:
+        print("Unable to add task")
+#===================================================================================================
+
+def edit_task(original_task_list, curr_user):
+    """Option to edit current user tasks"""
+    if not original_task_list:
+        print("Error: The list of dictionaries is empty.")
+        return
+
+    while True:
+        try:
+            # Creates a list of curr_user tasks for editing
+            this_user_tasks = [user for user in original_task_list if user['username'] in curr_user]
+            if not this_user_tasks:
+                print("No users found with the specified usernames.")
+                return
+
+            # Create a custom index based on length of curr_user tasks
+            display_index = [index for index in range(len(this_user_tasks) + 1)]
+            # Remove the 0 from start
+            display_index.pop(0)
+            # Build a table for display only
+            table = tabulate(this_user_tasks, showindex=display_index)
+            print(table)
+
+            # Ask the user to select a dictionary by index
+            index_to_edit = int(input("Enter the index of the dictionary to edit (1 to {}) or -1 to exit: ".format(len(this_user_tasks))))
+
+            if index_to_edit == -1:
+                print("Returned to main menu.")
+                return
+            # Ensure the index is valid
+            if 1 <= index_to_edit <= len(this_user_tasks):
+                selected_dict = this_user_tasks[index_to_edit - 1]
+                print("\nSelected dictionary:\n", selected_dict)
+
+                # Allow the user to edit the selected dictionary
+                key_to_edit = input("Enter the key to edit: ")
+                new_value = input(f"Enter the new value for key '{key_to_edit}': ")
+
+                #if key_to_edit == selected_dict.keys():
+                for key_in_dict in selected_dict.keys():
+                    if key_to_edit == key_in_dict:
+                        selected_dict[key_to_edit] = new_value
+                        print("Successfully updated the task.")
+                    else:
+                        print("\nUnable to add task. Please check spelling and try again.")
+
+                print("Dictionary updated:", selected_dict)
+                add_task_to_file(original_task_list, selected_dict)
+                return
+            else:
+                print("Invalid index. Please enter a valid index.")
+
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
 #===================================================================================================
 
 def view_all(all_tasks):
-    '''Reads the task from task.txt file and prints to the console
+    '''Reads all tasks from task.txt file and prints to the console
     '''
     if all_tasks == "":
         print("No current tasks.")
-
-    # for t in all_tasks:
-    #     disp_str = f"Task: \t\t {t['title']}\n"
-    #     disp_str += f"Assigned to: \t {t['username']}\n"
-    #     disp_str += f"Date Assigned: \t {t['assigned_date'].strftime(DATETIME_STRING_FORMAT)}\n"
-    #     disp_str += f"Due Date: \t {t['due_date'].strftime(DATETIME_STRING_FORMAT)}\n"
-    #     disp_str += f"Task Description: \n {t['description']}\n"
-        #print(disp_str)
     print(tabulate(all_tasks))
 #===================================================================================================
 
-def view_mine(curr_user, my_tasks):
-    '''Reads the task from task.txt file and prints to the console
+def view_mine(all_tasks, curr_user):
+    '''Reads user tasks from task.txt file and prints to the console
+    offers option to edit specific task by number and dictionary key
     '''
-    # choice = 1
-    # # for t in my_tasks:
-    # #     if t['username'] == curr_user:
-    # #         disp_str = f"Task: \t\t {t['title']}\n"
-    # #         disp_str += f"Assigned to: \t {t['username']}\n"
-    # #         disp_str += f"Date Assigned: \t {t['assigned_date'].strftime(DATETIME_STRING_FORMAT)}\n"
-    # #         disp_str += f"Due Date: \t {t['due_date'].strftime(DATETIME_STRING_FORMAT)}\n"
-    # #         disp_str += f"Task Description: \n {t['description']}\n"
-    #         #print(disp_str)
-    # print(tabulate(my_tasks))
-    
-    # total_tasks = len(my_tasks)
-    # if choice == 1:
-    #     print(my_tasks[0]['title'])
-    #     print(total_tasks)
+    if all_tasks == "":
+        print("No current tasks.")
+    edit_task(all_tasks, curr_user)
 #===================================================================================================
 
 def user_menu():
     """Main menu of choices for user"""
-    successful_log_in = user_log_in()
-    # Dictionary of current users and passwords
-    username_password = successful_log_in[0]
-    curr_user = successful_log_in[1]
+    # 
+    main_log_in = user_log_in() #"decclan"
+    #username_password = #"Yellowlemons$1"
+    curr_user = main_log_in[0]
+    all_users_dict = main_log_in[1]
 
-    #prints all username passwors dictionary
-    #print(username_password)
+    #prints all username passwords dictionary
+    print("curr_user is:\n")
+    print(all_users_dict)
+    print(curr_user)
 
     curr_tasks = task_file()
 
@@ -293,25 +348,21 @@ def user_menu():
     : ''').lower()
 
         if menu == 'r':
-            '''admin can add new users.'''
-            if curr_user == "admin":
-                reg_user(username_password)
-            else:
-                print("You do not have the required permissions to add a new user.")
+            reg_user(all_users_dict)
 
         elif menu == 'a':
-            add_task(username_password, curr_tasks)
+            add_task(all_users_dict, curr_tasks)
 
         elif menu == 'va':
             view_all(curr_tasks)
 
         elif menu == 'vm':
-            view_mine(curr_user, curr_tasks)
+            view_mine(curr_tasks, curr_user)
 
         elif menu == 'ds':
             '''admin can display statistics about number of users and tasks.'''
             if curr_user == 'admin':
-                num_users = len(username_password.keys())
+                num_users = len(all_users_dict.keys())
                 num_tasks = len(curr_tasks)
 
                 print("-----------------------------------")
@@ -328,7 +379,4 @@ def user_menu():
         else:
             print("You have made a wrong choice, Please Try again")
 #===================================================================================================
-
 user_menu()
-
-# regex for password - 1 captial letter, 1 number, 1 special character
